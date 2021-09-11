0452cd502e15fa8feecf9ed9e8b12504c44e9e72
Elis Marcela <82593291+develis@users.noreply.github.com>
2021-09-11 11:23:13 -0300

    configurando javascript e estilização

--- assets/js/script_professores.js
+++ assets/js/script_professores.js
@@ -1,17 +1,35 @@
 $(document).ready(function () {
     $('.login-dropdown').hide();
+
     var progresso = 1;
     var intervalId = window.setInterval(function () {
         progresso = $('input[type="checkbox"]:checked').length * 6.25;
         $('#barraProgresso').width(progresso + '%')
     }, 300);
+
     var click = 0;
-    $('#ativar-dropdown').click(function () { 
+    $('#ativar-dropdown').click(function () {
         click++;
-        if (click%2 == 0){
-            $('.login-dropdown').hide(); 
-        } else{
-            $('.login-dropdown').show(); 
+        if (click % 2 == 0) {
+            $('.login-dropdown').hide();
+        } else {
+            $('.login-dropdown').show();
         }
+    });
+
+    $('#video1unid1').click(function () {
+        $("#video").attr("src", "https://www.youtube.com/embed/vV196a3vaaY");
+    });
+    $('#video2unid1').click(function () {
+        $("#video").attr("src", "https://www.youtube.com/embed/lTwessnK0hQ");
+    });
+    $('#video3unid1').click(function () {
+        $("#video").attr("src", "https://www.youtube.com/embed/WZIGwN-5Ioo");
+    });
+    $('#video4unid1').click(function () {
+        $("#video").attr("src", "https://www.youtube.com/embed/DaPAqcpvUzA");
+    });
+    $('#video1unid2').click(function () {
+        $("#video").attr("src", "https://www.youtube.com/embed/fLWqIrmVJ2E");
     });
 });
